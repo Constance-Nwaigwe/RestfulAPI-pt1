4d4c9295206e898483645fbefcf9d2c4d7f7f410
Constance Nwaigwe <constancenwaigwe@Constances-MacBook-Pro.local>
2022-08-15 17:28:53 -0400

    created api server with sql

--- app.js
+++ app.js
@@ -2,21 +2,46 @@ const express = require("express");
 
 const app = express();
 
-const bookRouter = express.Router();
-
 const port = 4000;
 
-bookRouter.route("/books").get((req, res) => {
-  const response = { hello: "Hi there" };
-  res.json(response);
-});
+const { Users } = require("./api/class");
+const seed = require("./seed");
 
-app.use("/api", bookRouter);
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Welcome to RestfulAPI");
 });
 
-app.listen(port, () => {
-  console.log(`Running app at port ${port}`);
+//get all users
+app.get("/users", async (req, res) => {
+  const users = await Users.findAll();
+  res.json({ users });
+});
+//get user by id
+app.get("/users/:id", async (req, res) => {
+  const user = await Users.findByPk(req.params.id);
+  res.json({ user });
+});
+//post a new users
+app.post("/users", async (req, res) => {
+  const newuser = await Users.create(req.body);
+  res.json({ newuser });
+});
+
+//update user info
+app.put("/users/:id", async (req, res) => {
+  const user = await Users.update(req.body, { where: { id: req.params.id } });
+  res.json({ user });
+});
+
+//Delete user
+app.delete("/users/:id", async (req, res) => {
+  await Users.destroy({ where: { id: req.params.id } });
+  res.json({ message: "User deleted" });
+});
+
+app.listen(port, async () => {
+  await seed();
+  console.log(`Running app on port ${port} at http://localhost:${port}`);
 });
